ed5d5f676e0bea8d6f2e2b8a3586779759745232
Raphomet <Raphael.T.Lee@gmail.com>
2010-12-15 20:41:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gryffindor::Application.config.secret_token = '6e850c2bbd8ed7fd443b0306026618dd6211ac8325bd5052069e7007f787e1e1811c6dd7d56b199ce8f2df9766f0e8f67bdea885b25229b694f33fa06e92f11d'
