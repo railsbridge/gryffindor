599d4d19756ef06ab0bf8ac82c27fc3cac07cb27
kellydunn <defaultstring@gmail.com>
2011-02-11 19:27:31 -0800

    [model] Added a hackish attempt to display questions of certain types

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ActiveRecord::Base
   belongs_to :event
 
   validates_presence_of :question_text
+
+  # TODO evaluate type and determine correct helper tag to use
+  def self.input_tag(question, name)
+    return "<strong>#{question.question_text}</strong><br />
+            <input type='#{question.type.downcase}' name='#{name}'/>"
+  end
 end
