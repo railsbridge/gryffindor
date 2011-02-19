716f115adcd0360033989ebd136fb80fbfc90d2a
kellydunn <defaultstring@gmail.com>
2011-02-19 15:56:36 -0800

    [model] Adding in some STI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def question_tag(question)
+    question.print_tag
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,8 +5,6 @@ class Question < ActiveRecord::Base
   validates_presence_of :question_text
 
   # TODO evaluate type and determine correct helper tag to use
-  def self.input_tag(question, name)
-    return "<strong>#{question.question_text}</strong><br />
-            <input type='#{question.type.downcase}' name='#{name}'/>"
+  def print_tag
   end
 end
