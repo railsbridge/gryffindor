db3e36308128cc2c32af3fcac5133d74833ac311
kellydunn <defaultstring@gmail.com>
2011-03-05 19:47:50 -0800

    [view][model][sort-of-mega-commit] Dyanmic form input helpers are working\!

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,11 +5,11 @@ class Question < ActiveRecord::Base
   validates_presence_of :question_text
 
   def form_type
-    (self.class).to_s.slice(/(.)+(?=Question)/).downcase
+    self.class.to_s.underscore.slice(/(.)+(?=_question)/)
   end
 
   def options
-    (self.question_text.split(":")[1]).split(",")
+    self.question_text.split(":")[1].to_s.split(",")
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ event = Factory(:event)
 
 # Questions - Example data
 TextQuestion.create!({:question_text => "Do you know what a variable is?", :event_id => event.id})
-TextQuestion.create!({:question_text => "Have you used the command line?", :event_id => event.id})
-TextQuestion.create!({:question_text => "I can name 5 UNIX commands.", :event_id => event.id})
-TextQuestion.create!({:question_text => "How did you hear about this group?", :event_id => event.id})
-TextQuestion.create!({:question_text => "What do you want to learn from this workshop?", :event_id => event.id})
+BooleanQuestion.create!({:question_text => "Have you used the command line?", :event_id => event.id})
+RadioQuestion.create!({:question_text => "I can name __ Unix Commands : 1, 5, 10, 20, what is Unix?", :event_id => event.id})
+SelectQuestion.create!({:question_text => "I heard about this group from : Google, Facebook, Sarah Mei", :event_id => event.id})
+CheckBoxesQuestion.create!({:question_text => "I want to learn about : Rails, Ruby, How to start a business", :event_id => event.id})
 
 
