cc422ae59980f757c3f76d889ca83b7af27f2c21
kellydunn <defaultstring@gmail.com>
2011-03-07 11:42:07 -0800

    [spec][helpers] Added an event creation helper for capybara integration tests

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -1,1 +1,8 @@
-
+module EventHelper
+  def create_event(options = {})
+    fill_in "event_name", :with => (options[:name])? options[:name] : "test"
+    fill_in "event_capacity", :with => (options[:capacity])? optoins[:capacity] : 1
+    fill_in "event_questions_attributes_0_question_text", :with => (options[:question_text])? options[:question_text] : "test"
+    select (options[:type])? options[:type] : "TextQuestion", :from => "event_questions_attributes_0_type"
+  end
+end
--- spec/integration/event_spec.rb
+++ spec/integration/event_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Creating a new event" do
   include CoreHelper
+  include EventHelper
   
   before :each do
     @admin = login_as_admin
@@ -10,6 +11,11 @@ describe "Creating a new event" do
   it "should not be able to create an event without filling out all the fields" do
     visit new_event_path
     click_button "Save"
-    page.should have_content "error"
+    Event.find(:all).length.should == 0
+  end
+
+  it "should create the correct question type upon creation" do
+    visit new_event_path
+    create_event
   end
 end
