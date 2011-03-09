b5c269c7866008dcfbbe90f94b63b714b699cddd
kellydunn <defaultstring@gmail.com>
2011-03-09 12:58:36 -0800

    [spec] All green\!

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -2,7 +2,9 @@ module EventHelper
   def create_event(options = {})
     fill_in "event_name", :with => (options[:name])? options[:name] : "test"
     fill_in "event_capacity", :with => (options[:capacity])? optoins[:capacity] : 1
-    fill_in "event_questions_attributes_0_question_text", :with => (options[:question_text])? options[:question_text] : "test"
-    select (options[:type])? options[:type] : "TextQuestion", :from => "event_questions_attributes_0_type"
+    next_day = page.find(:css, "#event_end_time_1i")[:value].to_i + 1
+    select next_day.to_s, :from => "event[end_time(1i)]"
+    fill_in "event[questions_attributes][0][question_text]", :with => (options[:question_text])? options[:question_text] : "test"
+    select (options[:type])? options[:type] : "TextQuestion", :from => "event[questions_attributes][0][type]"
   end
 end
